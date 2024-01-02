cf804d7e33d684036f34d2515f6bab90ec9daf5b
dnezam <55559979+dnezam@users.noreply.github.com>
2024-01-02 08:19:50 +0100

    Add projectile binary

--- src/bin/projectile.rs
+++ src/bin/projectile.rs
@@ -1,5 +1,35 @@
 use raytracer::Tuple;
 
 fn main() {
-    println!("Hello {:?}", Tuple::point(0.0, 1.0, 4.2))
+    let mut p = Projectile {
+        position: Tuple::point(0.0, 1.0, 0.0),
+        velocity: Tuple::vector(1.0, 1.0, 0.0).normalize(),
+    };
+    let e = Environment {
+        gravity: Tuple::vector(0.0, -0.9, 0.0),
+        wind: Tuple::vector(-0.01, 0.0, 0.0),
+    };
+
+    while p.position.y > 0.0 {
+        println!("{:?}", p);
+        p = tick(e, p);
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Projectile {
+    position: Tuple, // point
+    velocity: Tuple, // vector
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Environment {
+    gravity: Tuple, // vector
+    wind: Tuple,    // vector
+}
+
+fn tick(env: Environment, proj: Projectile) -> Projectile {
+    let position = proj.position + proj.velocity;
+    let velocity = proj.velocity + env.gravity + env.wind;
+    Projectile { position, velocity }
 }
