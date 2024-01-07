4cc2e8889c9927e916f18fa562bdecbe13b46efa
dnezam <55559979+dnezam@users.noreply.github.com>
2024-01-07 08:12:31 +0100

    Think about and change how we use `as`

--- src/bin/clock.rs
+++ src/bin/clock.rs
@@ -3,19 +3,27 @@ use std::{f64::consts::PI, path::Path};
 use raytracer::{Canvas, Color, Matrix, Tuple};
 
 fn main() {
-    let width = 400;
-    let height = 400;
-    let (middle_x, middle_y) = (width / 2, height / 2);
+    let width = 300;
+    let height = 300;
+    // This panics if the canvas is too large for the point in the middle to be
+    // represented by i32. If this is a problem, decrease canvas size or change to
+    // i64.
+    let (middle_x, middle_y): (i32, i32) = (
+        (width / 2).try_into().unwrap(),
+        (height / 2).try_into().unwrap(),
+    );
     let radian_step = 2.0 * PI / 12.0;
 
     let mut canvas = Canvas::new(width, height);
     for i in 1..=12 {
-        let position = Matrix::rotation_z(radian_step * (i as f64)).scale(150.0, 150.0, 0.0)
+        let position = Matrix::rotation_z(radian_step * (i as f64)).scale(120.0, 120.0, 0.0)
             * Tuple::point(1.0, 0.0, 0.0);
-        canvas[[
-            ((middle_x as i32) + (position.x.round() as i32)) as usize,
-            ((middle_y as i32) - (position.y.round() as i32)) as usize,
-        ]] = Color::new(1.0, 0.0, 0.0);
+
+        // This panics if the canvas is too small for the point to be drawn. If this is a problem,
+        // increase the canvas size, or move the point closer to the origin.
+        let canvas_x = (middle_x + (position.x.round() as i32)).try_into().unwrap();
+        let canvas_y = (middle_y - (position.y.round() as i32)).try_into().unwrap();
+        canvas[[canvas_x, canvas_y]] = Color::new(1.0, 0.0, 0.0);
     }
 
     canvas
