018c16706e62c07866bc2b36b663fa4c6d8abc94
dnezam <55559979+dnezam@users.noreply.github.com>
2024-01-09 13:58:19 +0100

    Implement sphere transformations

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,11 +1,13 @@
 use crate::intersection::{Intersection, Intersections};
 use crate::utils::{self, Object};
-use crate::{ray::Ray, tuple::Tuple};
+use crate::{errors::MatrixError, ray::Ray, tuple::Tuple, Matrix};
+use std::result::Result;
 
 /// Represents a sphere located at the (world) origin.
 #[derive(Debug, Copy, Clone)]
 pub struct Sphere {
     id: usize,
+    transform: Matrix<4>,
 }
 
 impl PartialEq for Sphere {
@@ -19,11 +21,33 @@ impl Sphere {
     pub fn new() -> Self {
         Self {
             id: utils::get_id(),
+            transform: Matrix::<4>::IDENTITY,
+        }
+    }
+
+    /// Returns the current transformation.
+    pub fn transform(self) -> Matrix<4> {
+        self.transform
+    }
+
+    /// Sets the transformation matrix.
+    ///
+    /// # Returns
+    /// Returns Ok(()) if `transform` is an invertible matrix.
+    /// Returns Err(MatrixError::NotInvertible) if `transform` is not an invertible matrix.
+    pub fn set_transform(&mut self, transform: Matrix<4>) -> Result<(), MatrixError> {
+        if !transform.invertible() {
+            Err(MatrixError::NotInvertible)
+        } else {
+            self.transform = transform;
+            Ok(())
         }
     }
 
     /// Returns the t values where the ray with the sphere.
     pub fn intersect(self, ray: Ray) -> Intersections {
+        let ray = ray.transform(self.transform().inverse().unwrap());
+
         // Vector from sphere's center to the ray origin: Sphere is located at world origin
         let sphere_to_ray = ray.origin() - Tuple::point(0.0, 0.0, 0.0);
 
@@ -114,5 +138,52 @@ mod tests {
         assert_eq!(xs.len(), 2);
         assert_eq!(xs[0].object(), Object::Sphere(sphere));
         assert_eq!(xs[1].object(), Object::Sphere(sphere));
+    }
+
+    #[test]
+    fn default_transformation() {
+        let sphere = Sphere::new();
+        assert_eq!(sphere.transform(), Matrix::<4>::IDENTITY);
+    }
+
+    #[test]
+    fn change_transformation() {
+        let mut sphere = Sphere::new();
+        let t = Matrix::<4>::translation(2.0, 3.0, 4.0);
+        sphere.set_transform(t).unwrap();
+
+        assert_eq!(sphere.transform(), t);
+    }
+
+    #[test]
+    fn change_transformation_invalid() {
+        let mut sphere = Sphere::new();
+        let t = Matrix::<4>::default();
+        assert_eq!(
+            sphere.set_transform(t).unwrap_err(),
+            MatrixError::NotInvertible
+        );
+    }
+
+    #[test]
+    fn intersect_scaled_sphere() {
+        let r = Ray::new(Tuple::point(0.0, 0.0, -5.0), Tuple::vector(0.0, 0.0, 1.0)).unwrap();
+        let mut s = Sphere::new();
+        s.transform = Matrix::<4>::scaling(2.0, 2.0, 2.0);
+        let xs = s.intersect(r);
+
+        assert_eq!(xs.len(), 2);
+        assert_eq!(xs[0].t(), 3.0);
+        assert_eq!(xs[1].t(), 7.0);
+    }
+
+    #[test]
+    fn intersect_translated_sphere() {
+        let r = Ray::new(Tuple::point(0.0, 0.0, -5.0), Tuple::vector(0.0, 0.0, 1.0)).unwrap();
+        let mut s = Sphere::new();
+        s.transform = Matrix::<4>::translation(5.0, 0.0, 0.0);
+        let xs = s.intersect(r);
+
+        assert_eq!(xs.len(), 0);
     }
 }
